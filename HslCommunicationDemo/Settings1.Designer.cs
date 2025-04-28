9a9ccf35e4c4b955130a26684dfe9d45d8939443
cancelpj <9997452@qq.com>
2025-04-28 16:45:49 +0800

    依赖升级

--- HslCommunicationDemo/Settings1.Designer.cs
+++ HslCommunicationDemo/Settings1.Designer.cs
@@ -12,7 +12,7 @@ namespace HslCommunicationDemo {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings1 : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings1 defaultInstance = ((Settings1)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings1())));
